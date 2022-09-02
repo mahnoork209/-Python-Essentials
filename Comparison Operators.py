b7d00447681d8cc073b3d833f4bd6f2ef2d191b0
MAHNOOR <45254@gcslahore.edu.pk>
2022-09-03 00:34:13 +0500

    Add files via upload

---
+++ Comparison Operators.py
@@ -0,0 +1,22 @@
+#using equality operation
+j=10
+print( j==20)
+k=30
+print( k==30)
+#using inequality operator
+j=10
+print( j!=20)
+k=30
+print( k!=30)
+#using greater than operator
+n=50
+print(n>100 )
+#using less than operator
+n=50
+print(n<100 )
+#using greater than or equal to operator by taking input 
+n=int(input("Enter an integar :"))
+print(n>=100 )
+#using less than or equal to operator by taking input
+n=int(input("Enter an integar :"))
+print(n<=100 )
