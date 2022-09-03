2e3084c43257ae2b9c8804ad7309e5dc936dd28d
MAHNOOR <45254@gcslahore.edu.pk>
2022-09-04 00:48:26 +0500

    Add files via upload

---
+++ Nested Conditional Statement.py
@@ -0,0 +1,10 @@
+x = 10 #here use multiple numbers
+if x > 5:  
+    if x == 6:  
+        print("nested: x == 6")
+    elif x == 10: 
+        print("nested: x == 10")
+    else:
+        print("nested: else")
+else:
+    print("else")
