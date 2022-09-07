18b86515d746ee0d608554fa5c25b945d5a845fb
MAHNOOR <45254@gcslahore.edu.pk>
2022-09-07 22:00:24 +0500

    Update Example Collatz's hypothesis.py

--- Example Collatz's hypothesis.py
+++ Example Collatz's hypothesis.py
@@ -16,9 +16,9 @@ steps=0
 while c0!= 1:
     if c0 % 2 == 0:
        c0= int(c0/2) 
-    print(c0)
-else:
+       print(c0)
+    else:
        c0 =int( 3 *c0 +1)
        print(c0)
-       steps += 1    
+    steps += 1    
 print("Steps =",steps)
