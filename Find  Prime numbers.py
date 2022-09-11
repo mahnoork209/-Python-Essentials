b25db3a6f79b87425545b5c34b42dd2f7d7d99e6
MAHNOOR <45254@gcslahore.edu.pk>
2022-09-11 19:22:55 +0500

    Add files via upload

---
+++ Find  Prime numbers.py
@@ -0,0 +1,24 @@
+"""Your task is to write a function checking whether a number is prime or not.
+The function:
+is called is_prime;
+takes one argument (the value to check)
+returns True if the argument is a prime number, and False otherwise.
+Hint: try to divide the argument by all subsequent values (starting from 2) and check
+the remainder - if it's zero, your number cannot be a prime; think carefully about 
+when you should stop the process."""
+##############solution#########
+def is_prime(num):
+   prime_num=0
+   for i in range(2,num):
+       if (num % i) == 0:
+           prime_num=False
+           break
+       else:
+           prime_num = True
+   return prime_num
+
+
+for i in range(1, 20):
+	if is_prime(i + 1):
+			print(i + 1, end=" ")
+print()
